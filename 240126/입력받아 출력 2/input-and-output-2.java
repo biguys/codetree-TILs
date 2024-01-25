13ac655a39494da8011c417151d58f1a6fdad9e7
biguys <54903775+biguys@users.noreply.github.com>
2024-01-26 04:23:04 +0900

    Title: 입력받아 출력 2, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/입력받아 출력 2/input-and-output-2.java
+++ 240126/입력받아 출력 2/input-and-output-2.java
@@ -3,8 +3,16 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+        /* - useDelimiter -
         sc.useDelimiter("-");
         int a = sc.nextInt(), b = sc.nextInt();
         System.out.printf("%d%d",a,b);
+        */
+
+        String a = sc.next();
+        String[] b = a.split("-");
+        System.out.printf("%s%s",b[0],b[1]);
+        
+
     }
 }
