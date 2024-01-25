251dbf7e23cea5590ec0cba0de9b8c764869b331
biguys <54903775+biguys@users.noreply.github.com>
2024-01-26 05:05:11 +0900

    Title: 전화번호 바꾸기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/전화번호 바꾸기/change-phone-number.java
+++ 240126/전화번호 바꾸기/change-phone-number.java
@@ -4,9 +4,13 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        
+        /* - useDelimiter -
         sc.useDelimiter("-");
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
         System.out.printf("0%d-%d-%d",a,c,b);
+        */
+        String a = sc.next();
+        String[] b = a.split("-");
+        System.out.printf("%s-%s-%s",b[0],b[2],b[1]);
     }
 }
