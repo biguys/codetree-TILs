9041005fff1f5d88a9494ba022a89646b115dbea
biguys <54903775+biguys@users.noreply.github.com>
2024-02-01 23:48:11 +0900

    Title: 큰 수에서 빼기, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240201/큰 수에서 빼기/subtract-from-large-number.java
+++ 240201/큰 수에서 빼기/subtract-from-large-number.java
@@ -9,5 +9,8 @@ public class Main {
             System.out.printf("%d",a-b);
         if(a<b)
             System.out.printf("%d",b-a);
+        else
+            System.out.println("0");
+            
     }
 }
