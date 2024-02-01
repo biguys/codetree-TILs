1319a5409834cf3ed95482713d3468e56dceb055
biguys <54903775+biguys@users.noreply.github.com>
2024-02-02 00:32:30 +0900

    Title: 시험 통과 여부 확인하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/시험 통과 여부 확인하기/verify-test-passed.java
+++ 240202/시험 통과 여부 확인하기/verify-test-passed.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
 
         if(n>=80)
-            System.out.print(n);
+            System.out.print("pass");
         else
             System.out.printf("%d more score",80-n);
     }
