5ddf73894bb6f3fbb2e2d05f4f1ea56c049f3d02
biguys <54903775+biguys@users.noreply.github.com>
2024-02-02 00:39:14 +0900

    Title: 체질량지수, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240202/체질량지수/bmi.java
+++ 240202/체질량지수/bmi.java
@@ -5,10 +5,11 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int cm = sc.nextInt(), kg = sc.nextInt();
-        double bmi = (double) kg / (cm/100) * (cm/100);
-        if(bmi>25)
-        System.out.printf("%.0f\nObsity",(double)bmi);
-        else
-        System.out.printf("%.0f",(double)bmi);
+        int bmi = kg*100*100/(cm*cm);
+        
+        System.out.print(bmi);
+
+        if(bmi>=25)
+            System.out.print("oBesity");
     }
 }
