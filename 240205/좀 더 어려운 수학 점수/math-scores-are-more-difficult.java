d596bf005c7c845fb47b945cec4d5ee9c37b042a
biguys <54903775+biguys@users.noreply.github.com>
2024-02-05 16:00:42 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240205/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int am = sc.nextInt(), ae  = sc.nextInt(), bm  = sc.nextInt(), be = sc.nextInt();
+
+        if(am > bm)
+            System.out.print("A");
+        else System.out.print("B");
+        
+        if(am ==bm)
+            System.out.print();
+        else if (be>de ? 'A' : "B")
+            System.out.print("A");
+    }
+}
