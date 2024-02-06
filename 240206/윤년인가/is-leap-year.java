ad35475fb9aeeff8edc163faf9c204d4d0573daf
biguys <54903775+biguys@users.noreply.github.com>
2024-02-06 16:27:39 +0900

    Title: 윤년인가, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/윤년인가/is-leap-year.java
+++ 240206/윤년인가/is-leap-year.java
@@ -8,7 +8,9 @@ public class Main {
 
         if((y % 4 == 0 && y % 100 != 0) || (y % 4 == 0 && y % 100 == 0 && y % 400 == 0))
             System.out.print("true");
-        else if (y % 4 == 0 && y % 100 == 0)
+        else
             System.out.print("false");
+        
+            
     }
 }
