fcc79cb4789e9c336a9aac19c4504a1070c2903b
biguys <54903775+biguys@users.noreply.github.com>
2024-02-06 22:25:24 +0900

    Title: 일 수 구하기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240206/일 수 구하기/number-of-days-in-month.java
+++ 240206/일 수 구하기/number-of-days-in-month.java
@@ -8,7 +8,7 @@ public class Main {
 
         if(n==2)
             System.out.print("28");
-        else if(n==4 && n==6 && n==9 && n==11) 
+        else if(n==4 || n==6 || n==9 || n==11) 
             System.out.print("30");
         else
             System.out.print("31");
