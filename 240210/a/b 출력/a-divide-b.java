37eff0f568b9a805730950ec8530867f9916d2c4
biguys <54903775+biguys@users.noreply.github.com>
2024-02-10 03:18:31 +0900

    Title: a/b 출력, Time: 155ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240210/a/b 출력/a-divide-b.java
+++ 240210/a/b 출력/a-divide-b.java
@@ -5,10 +5,15 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt();
-        double answer;
+        int answer1 = (a*10) / b;
+        int answer2 = (a*10) % b;
 
-        answer = (double) a / b;
-        System.out.printf("%.20f", answer);
+        System.out.print("0."+answer1);
+        for(int i=0; i<19; i++)
+        {
+            answer2 += (answer2 / 10);
+            System.out.print(answer2);
+        }
 
     }
 }
