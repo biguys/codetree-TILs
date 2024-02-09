1a9a56e1e00ee1eef638777676b6110a0b709b40
biguys <54903775+biguys@users.noreply.github.com>
2024-02-10 03:58:31 +0900

    Title: a/b 출력, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240210/a/b 출력/a-divide-b.java
+++ 240210/a/b 출력/a-divide-b.java
@@ -5,15 +5,22 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt();
-        int answer1 = (a*10) / b;
-        int answer2 = (a*10) % b;
-        double answer3 = (double) a/b;
-        System.out.printf("%.0f.",answer3);
-        for(int n=1; n<=20; n++)
-        {
-            System.out.print(answer1);
-            answer1 = (answer2*10) / b;
-            answer2 = (answer2*10) % b; 
+        int answer1 = (a) / b;
+        int answer2 = (a) % b;
+        for(int n=1; n<=21; n++)
+        {   
+            if(n==1)
+            {
+                System.out.print(answer1+".");
+                answer1 = (answer2*10) / b;
+                answer2 = (answer2*10) % b; 
+            }
+            else
+            {
+                System.out.print(answer1);
+                answer1 = (answer2*10) / b;
+                answer2 = (answer2*10) % b; 
+            }
         }
 
     }
