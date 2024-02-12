9f3d3a8e7c4c45100d4fc5632f5dbf5205d32272
biguys <54903775+biguys@users.noreply.github.com>
2024-02-12 23:53:29 +0900

    Title: 합성수 판별기, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240212/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean a = false;
+
+        for(int i=1; i<=n; i++)
+        {
+            if(n % 2 == 0 || n % 2 == 1 || n % 3 == 0)
+                a = true;
+        }
+
+        if(a == true)
+            System.out.print("C");
+        else
+            System.out.print("N");
+    }
+}
